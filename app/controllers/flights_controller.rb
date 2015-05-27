faea8690e24c6c15ed3c6512e18f2f167771f3f9
spectre6000 <spectre6000@gmail.com>
2015-05-27 14:29:59 -0600

    Search function

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,10 @@
 class FlightsController < ApplicationController
 
   def index
-    @airports = Airport.all.map{ |x| x.code}
+    @flights = Flight.search(params)
+    @airports = Airport.all.map{ |x| [x.code, x.id] }
     @passengers = [1,2,3,4].map
-    @dates = Flight.all.map{ |z| z.date.strftime("%m/%d/%Y") }.uniq
+    @dates = Flight.all.map{ |z| [z.date.strftime("%m/%d/%Y"), z.date] }
   end
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,18 @@
 class Flight < ActiveRecord::Base
   belongs_to :origin, class_name: "Airport"
   belongs_to :destination, class_name: "Airport"
+
+  def self.search(params)
+    if params && !params[:date].nil?
+      Flight.on_date(params[:date])
+        .where(:origin => params[:origin], :destination_id => params[:destination])
+        # .includes(:origin, :destination)
+    end
+  end
+
+  def self.on_date(day)
+    # elegant solution from: http://stackoverflow.com/questions/2381718/rails-activerecord-date-between
+    where(:date => day.to_date.beginning_of_day..day.to_date.end_of_day)
+  end
+
 end
