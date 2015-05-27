2196850f924299dddda75c65921bea97c4771e81
spectre6000 <spectre6000@gmail.com>
2015-05-27 14:53:22 -0600

    Add booking/passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527204520) do
+ActiveRecord::Schema.define(version: 20150527205259) do
 
   create_table "airports", force: :cascade do |t|
     t.string   "code"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20150527204520) do
     t.integer  "destination_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
