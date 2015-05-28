7cae90a3db474892d5d4bc8a9bbd3b8bcfaae39e
spectre6000 <spectre6000@gmail.com>
2015-05-28 09:54:32 -0600

    Booking commit/show, passenger creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,14 +3,26 @@ class BookingsController < ApplicationController
   def new
     @flight = Flight.find_by(params[:flight_id])
     @booking = @flight.bookings.build
-    params[:passengers].to_i.times {@booking.passengers.build}
-    
+    params[:passengers].to_i.times {@booking.passengers.build}    
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      render 'show'
+    else
+      render 'new'
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
+
+  private
+
+    def booking_params
+      params.require(:booking).permit(:flight_id, :passengers_attributes => [:name, :email] )
+    end
 
 end
